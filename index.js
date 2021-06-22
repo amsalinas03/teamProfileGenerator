d7d1636292b47e3885f0a0a022c466fffee94990
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-06-21 22:16:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 import { Employee, Engineer, Intern, Manager } from "./models/employeeModels";
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+function mainMenu() {
+    function newManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "Please enter a manager's name",
+                validate: answer => {
+                    if (answer.length == 0) {
+                        return "Please enter a valid name"
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "Please enter the manager's ID number"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "Please enter the manager's email"
+            },
+        ])
+    }
+}
