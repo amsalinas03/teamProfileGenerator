dea290fd59f0accfd0eeb3a3fbaf1c2bddb0e716
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-06-21 22:08:22 -0500

    created index.js and imported necessities

---
+++ index.js
@@ -0,0 +1,3 @@
+import { Employee, Engineer, Intern, Manager } from "./models/employeeModels";
+const inquirer = require("inquirer");
+const fs = require("fs");
--- models/employeeModels.js
+++ models/employeeModels.js
@@ -1,24 +1,24 @@
-function Employee (name, id, email) {
+export function Employee (name, id, email) {
     this.name = name,
     this.id = id,
     this.email = email
 }
 
-function Engineer (name, id, email, github) {
+export function Engineer (name, id, email, github) {
     this.name = name,
     this.id = id,
     this.email = email,
     this.github = github
 }
 
-function Intern (name, id, email, school) {
+export function Intern (name, id, email, school) {
     this.name = name,
     this.id = id,
     this.email = email,
     this.school = school
 }
 
-function Manager (name, id, email, officeNumber) {
+export function Manager (name, id, email, officeNumber) {
     this.name = name,
     this.id = id,
     this.email = email,
