4bcc8e9871654cb7efdaec45701c673fcc2ae355
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-06-21 22:02:19 -0500

    Create employeeModels.js

---
+++ models/employeeModels.js
@@ -0,0 +1,26 @@
+function Employee (name, id, email) {
+    this.name = name,
+    this.id = id,
+    this.email = email
+}
+
+function Engineer (name, id, email, github) {
+    this.name = name,
+    this.id = id,
+    this.email = email,
+    this.github = github
+}
+
+function Intern (name, id, email, school) {
+    this.name = name,
+    this.id = id,
+    this.email = email,
+    this.school = school
+}
+
+function Manager (name, id, email, officeNumber) {
+    this.name = name,
+    this.id = id,
+    this.email = email,
+    this.officeNumber = officeNumber
+}
